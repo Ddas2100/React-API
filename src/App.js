d97ae8097ef58acd2fdfbb29c0c0fb87850c0bf1
Debjyoti Das <ddas2100@gmail.com>
2023-12-26 23:20:50 +0530

    eCommerce_Task7-Loading the application
    When the user clicks on the fetch button, a loader will be shown on the screen while fetching the data from the backend and Once the data has come remove the loader.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React, { useState } from 'react';
-
+import loading from './assets/loading.gif';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  function fetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films/')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
+    const response = await fetch('https://swapi.dev/api/films/');
+    const data= await response.json();
+    
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -30,7 +30,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} /> }
+        {isLoading && <img src= {loading} alt='loading gif' />}
       </section>
     </React.Fragment>
   );
