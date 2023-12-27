c3b8ff160157fab3af0aa7cf631d7357767e3367
Debjyoti Das <ddas2100@gmail.com>
2023-12-28 03:27:10 +0530

    eCommerce_Task10-Adding new Movies into the list
    Created a form that consists three input boxes- Movie Name, Movie Details, and Release Date, on the top of the fetch movie button to Add Movie. When the user clicks on the Add Movie button, show all that movie details as an object in the browser console by doing console.log(Movie). Also the input boxes should become clear again and display the newly added movie details in the same section where the fetch movie results are displayed.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import loading from './assets/loading.gif';
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -11,15 +12,14 @@ function App() {
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
-
     try {
       const response = await fetch('https://swapi.dev/api/films/');
-      if(!response.ok) {
-        throw new Error('Something Went Wrong!');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
       }
 
-      const data= await response.json();
-      
+      const data = await response.json();
+
       const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
@@ -39,24 +39,34 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  let content = <p>No Movies Found</p>;
+  function addMovieHandler(movie) {
+    console.log(movie);
+    setMovies((prevMovies) => [...prevMovies, movie]);;
+  }
 
-  if(movies.length > 0) {
-    content= <MoviesList movies={movies} />;
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
   }
-  if(error) {
-    content= <p>{error}</p>;
+
+  if (error) {
+    content = <p>{error}</p>;
   }
-  if(isLoading) {
-    content= <img src= {loading} alt='loading gif' />;
+
+  if (isLoading) {
+    content = <img src={loading} alt="loading gif" />;
   }
 
   return (
     <React.Fragment>
       <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section> {content} </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
