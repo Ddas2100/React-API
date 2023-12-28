cd4743c279d94731abd3049368d4d71b6e79a803
Debjyoti Das <ddas2100@gmail.com>
2023-12-29 01:18:47 +0530

    eCommerce_Task11-Storing and deleting movies
    Using POST request we will store newly added movie details through the 'add form' into a backend database called Firebase and we will fetch that data using GET request and will show in the UI as a list. We will also add a delete button under each movie detail and upon clicking on it, that particular movie will be deleted both from the UI and backend database. It will be done using DELETE Api.

--- src/App.js
+++ src/App.js
@@ -13,22 +13,24 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-http-70c9a-default-rtdb.firebaseio.com/movies.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        })
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -39,15 +41,50 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
-    setMovies((prevMovies) => [...prevMovies, movie]);;
-  }
+  const addMovieHandler = useCallback(async(movie) => {
+    // console.log(movie);
+    // setMovies((prevMovies) => [...prevMovies, movie]);
+    try {
+      const response= await fetch('https://react-http-70c9a-default-rtdb.firebaseio.com/movies.json', {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: {
+          'Content-Type' : 'application/json'
+        }
+      });
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
 
-  let content = <p>Found no movies.</p>;
+      const data= await response.json();
+      console.log(data);
+      fetchMoviesHandler();
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, [fetchMoviesHandler]);
+
+  const deleteMovieHandler = useCallback(async (movieId) => {
+    try {
+      const response = await fetch(`https://react-http-70c9a-default-rtdb.firebaseio.com/movies/${movieId}.json`, {
+        method: 'DELETE',
+      });
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      await fetchMoviesHandler(); 
+    } catch (error) {
+      setError(error.message);
+    }
+  }, [fetchMoviesHandler]);
+
+  let content = <p>No Movies Found</p>;
 
   if (movies.length > 0) {
-    content = <MoviesList movies={movies} />;
+    content = <MoviesList movies={movies} onDeleteMovie={deleteMovieHandler} />;
   }
 
   if (error) {
--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import classes from './AddMovie.module.css';
 
 function AddMovie(props) {
@@ -6,40 +6,63 @@ function AddMovie(props) {
   const openingTextRef = useRef('');
   const releaseDateRef = useRef('');
 
+  const [isFormExpanded, setIsFormExpanded] = useState(false);
+  const [error, setError] = useState(null);
+
   function submitHandler(event) {
     event.preventDefault();
 
+    const title = titleRef.current.value.trim();
+    const openingText = openingTextRef.current.value.trim();
+    const releaseDate = releaseDateRef.current.value.trim();
+
+    if (!title || !openingText || !releaseDate) {
+      setError('Fill up the boxes with relevant details');
+      setTimeout(() => {
+        setError(null);
+      }, 3000);
+      return;
+    }
+
     const movie = {
-      title: titleRef.current.value,
-      openingText: openingTextRef.current.value,
-      releaseDate: releaseDateRef.current.value,
+      title,
+      openingText,
+      releaseDate,
     };
 
-    // Pass the movie object to the parent component
     props.onAddMovie(movie);
 
-    // Clear the form after adding the movie
     titleRef.current.value = '';
     openingTextRef.current.value = '';
     releaseDateRef.current.value = '';
+
+    setIsFormExpanded(false);
   }
 
   return (
-    <form onSubmit={submitHandler}>
-      <div className={classes.control}>
-        <label htmlFor='title'>Title</label>
-        <input type='text' id='title' ref={titleRef} />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor='opening-text'>Opening Text</label>
-        <textarea rows='5' id='opening-text' ref={openingTextRef}></textarea>
-      </div>
-      <div className={classes.control}>
-        <label htmlFor='date'>Release Date</label>
-        <input type='text' id='date' ref={releaseDateRef} />
-      </div>
-      <button type="submit">Add Movie</button>
-    </form>
+    <div>
+      <button onClick={() => setIsFormExpanded(!isFormExpanded)}>
+        {isFormExpanded ? 'Collapse Form' : 'Add Movie'}
+      </button>
+      {isFormExpanded && (
+        <form onSubmit={submitHandler}>
+          <div className={`${classes.control} ${error ? classes.error : ''}`}>
+            <label htmlFor='title'>Title</label>
+            <input type='text' id='title' ref={titleRef} />
+          </div>
+          <div className={`${classes.control} ${error ? classes.error : ''}`}>
+            <label htmlFor='opening-text'>Opening Text</label>
+            <textarea rows='5' id='opening-text' ref={openingTextRef}></textarea>
+          </div>
+          <div className={`${classes.control} ${error ? classes.error : ''}`}>
+            <label htmlFor='date'>Release Date</label>
+            <input type='text' id='date' ref={releaseDateRef} />
+          </div>
+          {error && <p style={{ color: 'red', fontWeight: 'bold' }}>{error}</p>}
+          <button type="submit">Add Movie</button>
+        </form>
+      )};
+    </div>
   );
 }
 export default AddMovie;
